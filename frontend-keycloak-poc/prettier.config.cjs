94dba790733ef75dbde9a2a84ac852d109d33b48
Stefan Stevic <steviss@gmail.com>
2023-06-10 12:31:49 +0200

    chore(setup-frontend-keycloak): add packages for the app, refactor config files to cjs

---
+++ frontend-keycloak-poc/prettier.config.cjs
@@ -0,0 +1,20 @@
+module.export = {
+  "printWidth": 140,
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": false,
+  "singleQuote": true,
+  "importOrder": [
+    "^src/types|src/utilities(.*)$",
+    "^src/components/store(.*)$",
+    "^src/pages(.*)$",
+    "^src/components/organisms(.*)$",
+    "^src/components/molecules(.*)$",
+    "^src/components/atoms(.*)$",
+    "^src/store(.*)$",
+    "^[./]"
+  ],
+  "importOrderSeparation": true,
+  "importOrderSortSpecifiers": true,
+  "plugins": ["@trivago/prettier-plugin-sort-imports"]
+}
